d8b820f3c74e29a1e3e57bb105546178c1222a4d
Yolanda84 <yolandamichaels84@yahoo.com>
2019-08-13 12:58:18 +0300

    tic tac toh button click

---
+++ TicTacToh/TicTacToh.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TicTacToh {
+    partial class TicTacToh {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(16, 20);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 100);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(122, 20);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 100);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(228, 20);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(100, 100);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(16, 126);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(100, 100);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(122, 126);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(100, 100);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(228, 126);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(100, 100);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(16, 232);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(100, 100);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(122, 232);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(100, 100);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(228, 232);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(100, 100);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // TicTacToh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 361);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TicTacToh";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToh";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+    }
+}
+
---
+++ TicTacToh/TicTacToh.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToh {
+    public partial class TicTacToh : Form {
+
+        private string _currentPLayer = "X";
+        
+        public TicTacToh() {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, EventArgs e) {
+
+            Button sendr = sender as Button;
+
+            sendr.Text = _currentPLayer;
+            sendr.Enabled = false;
+
+            if (_currentPLayer == "X") {
+                _currentPLayer = "O";
+            }
+            else if (_currentPLayer == "O") {
+                _currentPLayer = "X";
+            }
+            
+
+            //string[] ticTacTohh = { "X", "O"};
+            //int i = Array.IndexOf(ticTacTohh, sendr.Text);
+            //if (i >= 0 && i < 2) {
+            //    sendr.Text = ticTacTohh[1];
+            //}
+            //else {
+            //    sendr.Text = ticTacTohh[0];
+            //}
+
+
+        }
+
+
+    }
+}
