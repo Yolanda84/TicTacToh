23fe613396e351a4b7eb08c0b41dff9535954cfc
Yolanda84 <yolandamichaels84@yahoo.com>
2019-08-17 21:57:55 +0300

    two player mode

--- TicTacToh/TicTacToh.cs
+++ TicTacToh/TicTacToh.cs
@@ -12,38 +12,93 @@ namespace TicTacToh {
     public partial class TicTacToh : Form {
 
         private string _currentPLayer = "X";
-        
+        private int _counter = 0;
+
+
         public TicTacToh() {
             InitializeComponent();
         }
 
         private void ButtonClick(object sender, EventArgs e) {
 
+             _counter ++;
+            
             Button sendr = sender as Button;
 
+            sendr.Font = new Font(sendr.Font.Name, 18, sendr.Font.Style & FontStyle.Bold);
+            sendr.ForeColor = Color.Red;
+            
             sendr.Text = _currentPLayer;
+            ValidateTicTaToh();
             sendr.Enabled = false;
-
+              
+            
             if (_currentPLayer == "X") {
                 _currentPLayer = "O";
             }
             else if (_currentPLayer == "O") {
                 _currentPLayer = "X";
             }
-            
-
-            //string[] ticTacTohh = { "X", "O"};
-            //int i = Array.IndexOf(ticTacTohh, sendr.Text);
-            //if (i >= 0 && i < 2) {
-            //    sendr.Text = ticTacTohh[1];
-            //}
-            //else {
-            //    sendr.Text = ticTacTohh[0];
-            //}
-
-
+            DrawMessage();
         }
 
+        public void ValidateTicTaToh() {
 
+            if (btn1.Text == _currentPLayer && btn2.Text == _currentPLayer && btn3.Text == _currentPLayer) {
+
+                Congrats();
+            }
+
+            else if (btn4.Text == _currentPLayer && btn5.Text == _currentPLayer && btn6.Text == _currentPLayer) {
+
+                Congrats();
+
+            }
+
+            else if (btn7.Text == _currentPLayer && btn8.Text == _currentPLayer && btn9.Text == _currentPLayer) {
+
+                Congrats();
+            }
+
+            else if (btn1.Text == _currentPLayer && btn4.Text == _currentPLayer && btn7.Text == _currentPLayer) {
+
+                Congrats();
+            }
+
+            else if (btn2.Text == _currentPLayer && btn5.Text == _currentPLayer && btn8.Text == _currentPLayer) {
+
+                Congrats();
+            }
+
+            else if (btn3.Text == _currentPLayer && btn6.Text == _currentPLayer && btn9.Text == _currentPLayer) {
+
+                Congrats();
+            }
+
+            else if (btn1.Text == _currentPLayer && btn5.Text == _currentPLayer && btn9.Text == _currentPLayer) {
+
+                Congrats();
+            }
+
+            else if (btn3.Text == _currentPLayer && btn5.Text == _currentPLayer && btn7.Text == _currentPLayer) {
+                Congrats();
+            }
+        }
+
+        private void Congrats() {
+            MessageBox.Show("Congratulations, " + _currentPLayer + " wins");
+            this.Close();
+            TicTacToh ticTacToh = new TicTacToh();
+            ticTacToh.ShowDialog();
+        }
+
+        private void DrawMessage() {
+
+            if (_counter == 9) {
+                MessageBox.Show("You lost, suckers");
+
+            }
+
+        }
     }
 }
