fd5d35038ccd7b829208de209ba4b24f2bd96b40
Yolanda84 <yolandamichaels84@yahoo.com>
2019-08-18 19:50:33 +0300

    color change

--- TicTacToh/TicTacToh.cs
+++ TicTacToh/TicTacToh.cs
@@ -20,26 +20,35 @@ namespace TicTacToh {
         }
 
         private void ButtonClick(object sender, EventArgs e) {
-
-             _counter ++;
-            
             Button sendr = sender as Button;
 
-            sendr.Font = new Font(sendr.Font.Name, 18, sendr.Font.Style & FontStyle.Bold);
-            sendr.ForeColor = Color.Red;
-            
-            sendr.Text = _currentPLayer;
-            ValidateTicTaToh();
-            sendr.Enabled = false;
-              
-            
-            if (_currentPLayer == "X") {
-                _currentPLayer = "O";
+            if ((sendr.Text == "X") || (sendr.Text == "O")) {
+                return;
             }
-            else if (_currentPLayer == "O") {
-                _currentPLayer = "X";
+            else {
+                _counter++;
+                sendr.Font = new Font(sendr.Font.Name, 18, sendr.Font.Style & FontStyle.Bold);
+
+                sendr.Text = _currentPLayer;
+
+                if (_currentPLayer == "X") {
+                    sendr.ForeColor = Color.Red;
+                }
+                else if (_currentPLayer == "O") {
+                    sendr.ForeColor = Color.Blue;
+                }
+
+                ValidateTicTaToh();
+
+                if (_currentPLayer == "X") {
+                    _currentPLayer = "O";
+                    }
+                else if (_currentPLayer == "O") {
+                    _currentPLayer = "X";
+                }
+
+                DrawMessage();
             }
-            DrawMessage();
         }
 
         public void ValidateTicTaToh() {
@@ -48,38 +57,31 @@ namespace TicTacToh {
 
                 Congrats();
             }
-
             else if (btn4.Text == _currentPLayer && btn5.Text == _currentPLayer && btn6.Text == _currentPLayer) {
 
                 Congrats();
 
             }
-
             else if (btn7.Text == _currentPLayer && btn8.Text == _currentPLayer && btn9.Text == _currentPLayer) {
 
                 Congrats();
             }
-
             else if (btn1.Text == _currentPLayer && btn4.Text == _currentPLayer && btn7.Text == _currentPLayer) {
 
                 Congrats();
             }
-
             else if (btn2.Text == _currentPLayer && btn5.Text == _currentPLayer && btn8.Text == _currentPLayer) {
 
                 Congrats();
             }
-
             else if (btn3.Text == _currentPLayer && btn6.Text == _currentPLayer && btn9.Text == _currentPLayer) {
 
                 Congrats();
             }
-
             else if (btn1.Text == _currentPLayer && btn5.Text == _currentPLayer && btn9.Text == _currentPLayer) {
 
                 Congrats();
             }
-
             else if (btn3.Text == _currentPLayer && btn5.Text == _currentPLayer && btn7.Text == _currentPLayer) {
                 Congrats();
             }
